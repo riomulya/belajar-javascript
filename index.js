e78a9d8a92797a96ee0fa086774b7404268adaaf
riomulya <riomulya75@gmail.com>
2021-11-09 14:20:15 +0700

    tugas 1 javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+var message = "hello coding";
+
+let msg1 ="hello world";
+
+const msg2 ="hello semua";
+
+console.log(msg1)
---
+++ tugas1.js
@@ -0,0 +1,9 @@
+var nama1 ="RIO";
+
+let nama2 = "MULYA SYAWAL";
+
+const umur ="18";
+
+console.log("nama depan saya : " + nama1)
+console.log("nama belakang saya : " + nama2)
+console.log("umur saya adalah : " + umur + " tahun")
