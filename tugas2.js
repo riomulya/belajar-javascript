6d72941d76389918642ff46bbed293593eb54490
riomulya <riomulya75@gmail.com>
2021-11-10 03:54:12 +0700

    tugas 2 javascript

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var number = 2;
+var string = "rio";
+var bool = false;
+var nul = null;
+var array = [9,3,4,6,8,3,6];
+
+console.log(number)
+console.log(string)
+console.log(bool)
+console.log(nul)
+console.log(array)
