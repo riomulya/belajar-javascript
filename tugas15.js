74c8f78620b9565971fd3cf166fcf1a24cdd33de
riomulya <riomulya75@gmail.com>
2021-11-12 05:53:51 +0700

    tugas 15 javascript

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+function segitigasiku() {
+    var ab = 8;
+    var bc = 6;
+    console.log("nilai AB : ", ab)
+    console.log("nilai BC : ", bc)
+    console.log("panjang sisi ac pada segitiga siku-siku tersebut adalah :",
+        Math.sqrt(Math.pow(ab, 2) + Math.pow(bc, 2)),
+        "cm")
+}
+
+segitigasiku()
