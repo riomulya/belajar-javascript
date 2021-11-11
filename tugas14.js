43a9e68415451453c866762f0e8e33288c410ca4
riomulya <riomulya75@gmail.com>
2021-11-12 04:50:29 +0700

    tugas 14 javascript

---
+++ tugas14.js
@@ -0,0 +1,10 @@
+//ceil ,floor ,round
+
+var x = 37 / 7;
+console.log(x)
+console.log("===========")
+console.log(Math.ceil(x))
+console.log("===========")
+console.log(Math.floor(x))
+console.log("===========")
+console.log(Math.round(x))
