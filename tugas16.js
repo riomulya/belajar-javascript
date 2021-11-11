49724f9e2298cf627a6efe0a01a0f0439788ca3d
riomulya <riomulya75@gmail.com>
2021-11-12 05:54:19 +0700

    tugas 16 javascript

---
+++ tugas16.js
@@ -0,0 +1,9 @@
+function minmax() {
+    var x = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+    console.log("nilai minimal-nya adalah : ", x)
+    var x = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+    console.log("<===============================>")
+    console.log("nilai maksimal-nya adalah : ", x)
+}
+
+minmax()
