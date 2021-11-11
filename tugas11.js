987aa08e3b05509e9bf35fe1965600405baecabb
riomulya <riomulya75@gmail.com>
2021-11-11 14:36:31 +0700

    tugas 11 javascript

---
+++ tugas11.js
@@ -0,0 +1,6 @@
+//buatlah sebuah perhitungan dengan penjumlahan (+) , perkalian (*) dan pembagian (/)
+// Gunakan lah 4 angka ini 2, 4, 5 dan 10 sehingga menghasilkan output 25
+
+var hitung = (4 - 2) * 10 + 5;
+
+console.log(hitung)
