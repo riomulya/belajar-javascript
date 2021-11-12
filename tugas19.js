25300e9acbc6c0880afa130a834bed4bc67d875f
riomulya <riomulya75@gmail.com>
2021-11-13 06:44:21 +0700

    tugas 19 javascript

---
+++ tugas19.js
@@ -0,0 +1,14 @@
+function ulang() {
+    var kata = "Semangat....";
+    console.log(kata.repeat(5))
+    console.log("*****".repeat(1))
+    console.log("*****".repeat(4))
+    console.log("*****".repeat(6))
+    console.log("*****".repeat(8))
+    console.log("*****".repeat(12))
+    console.log("*****".repeat(8))
+    console.log("*****".repeat(6))
+    console.log("*****".repeat(4))
+    console.log("*****".repeat(1))
+}
+ulang()
