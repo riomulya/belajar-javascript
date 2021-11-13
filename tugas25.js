bfc5d337565456711e0eec47c67f3c2707469a02
riomulya <riomulya75@gmail.com>
2021-11-14 02:40:35 +0700

    tugas 25 javascript

---
+++ tugas25.js
@@ -0,0 +1,5 @@
+var angka = [2, 39, 76, 50, 9, 7, 41, 2, 24, 1, 16]
+console.log("Sebelumnya : ", angka)
+console.log("Ascending : ", angka.sort())
+console.log("Descending : ", angka.reverse())
+console.log("Filter > 10 : ", angka.filter(angka => angka > 10))
