82386c1e813f7ec1aa9bdbf62f04b14393b51054
riomulya <riomulya75@gmail.com>
2021-11-13 08:34:29 +0700

    tugas 20 javascript

---
+++ tugas20.js
@@ -0,0 +1,8 @@
+function cari() {
+    var kalimat = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable";
+    console.log(kalimat.match("are"))
+    console.log(kalimat.match("arer"))
+    console.log(kalimat.match("words"))
+}
+
+cari()
