2d7e2e7fe31ed7012eeef248762fa9a6f0f4fcc2
riomulya <riomulya75@gmail.com>
2021-11-14 08:01:45 +0700

    tugas 27 javscript

---
+++ tugas27.js
@@ -0,0 +1,8 @@
+function panggil() {
+    var kata1 = ["No", "rumah saya", 30, "A"]
+    var kata2 = ["Jalan pangeran Tirtayasa No", 70]
+    console.log(String(kata1))
+    console.log(String(kata2))
+}
+
+panggil()
